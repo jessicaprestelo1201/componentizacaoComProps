f0ba27716b96c13411a61c1fd3cc22637a63bce4
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2025-04-12 00:52:06 -0300

    feat: criação de componente footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
+
+      <Footer />
     </div>
   );
 }
