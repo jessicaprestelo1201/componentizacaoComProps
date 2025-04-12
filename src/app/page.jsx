e681d1d97b647fbea3eeae0cb0db1bcb5fe0efe6
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2025-04-12 01:03:01 -0300

    feat: criação de componente categoriesSection

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,14 @@ import styles from "./page.module.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import HeroSection from "./components/heroSection";
+import CategoriesSection from "./components/categoriesSection";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
       <HeroSection />
-
+      <CategoriesSection />
       <Footer />
     </div>
   );
