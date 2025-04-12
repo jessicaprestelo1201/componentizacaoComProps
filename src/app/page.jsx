6f2018b56551bde10e51ec1f6bc001380eccd48c
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2025-04-12 00:49:46 -0300

    feat: criação de componente header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "./components/header";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-
+      <Header />
     </div>
   );
 }
