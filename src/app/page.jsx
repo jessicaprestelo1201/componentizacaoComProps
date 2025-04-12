a1adaf25cfbd850f2bb7a39d838130734879f733
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2025-04-12 00:58:55 -0300

    feat: criação de componente heroSection

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import HeroSection from "./components/heroSection";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
+      <HeroSection />
 
       <Footer />
     </div>
